e08be408cf10b6ace02aaf5648a7d26eafc18e4e
Tu7mao <54181694+tuimao615@users.noreply.github.com>
2023-03-28 16:53:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1yCq6ImnaihAxQDskPUMqShyLuU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'AJW5zuJkomMYSLlBejTfWSL2dEggJ68JlXDvCgJHLJE',
+      useTemplateId: 'P4s0ds8iHPbMcLzyzJEGSq9CI3hvKe011tqLaLyieIc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-03',
       festivals: [
@@ -45,7 +45,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'AJW5zuJkomMYSLlBejTfWSL2dEggJ68JlXDvCgJHLJE',
+  CALLBACK_TEMPLATE_ID: 'P4s0ds8iHPbMcLzyzJEGSq9CI3hvKe011tqLaLyieIc',
 
   CALLBACK_USERS: [
     {
