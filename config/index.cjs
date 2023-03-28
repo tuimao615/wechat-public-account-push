9038c72b90fa9524ff5c1a6f000d08885fb3c75c
Tu7mao <54181694+tuimao615@users.noreply.github.com>
2023-03-28 16:49:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,26 +14,22 @@ const USER_CONFIG = {
   APP_SECRET: '51e348e1ee0bea4d73e60f1dc5e2f810',
 
   PROVINCE: '安徽',
-  CITY: '芜湖',
+  CITY: '亳州',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '亲爱的娴贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o1yCq6AVt7_eY_HwqsvWa9lOn_bk',
+      id: 'o1yCq6ImnaihAxQDskPUMqShyLuU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'AJW5zuJkomMYSLlBejTfWSL2dEggJ68JlXDvCgJHLJE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '06-15',
+      horoscopeDate: '02-03',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1999', date: '06-15',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1999', date: '05-02',
+          type: '*生日', name: '叶淑娴', year: '2000', date: '02-03',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '11-15',
@@ -43,8 +39,6 @@ const USER_CONFIG = {
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-01-26' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
